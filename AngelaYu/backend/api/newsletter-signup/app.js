478c419c092d99f9d0b6ec46472b27a74cd1f8c5
Jonathan VALERE <jonathan.valere@gmail.com>
2021-02-19 17:49:03 -0300

    mailchimp

--- AngelaYu/backend/api/newsletter-signup/app.js
+++ AngelaYu/backend/api/newsletter-signup/app.js
@@ -4,10 +4,24 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const request = require('request');
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/",(req,res)=>{
-	res.send('hello');
+	res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req,res)=>{
+	const { firstName, lastName, email } = req.body;
+	res.send(`${firstName} ${lastName}`);
 })
 
 app.listen(port,()=>{
 	console.log(`Server is running on ${port}`);
 })
+
+// api key
+// 7cd38b0ef17edbceac40d836662d7549-us7
+
+// list id
+// e6d9cddb7a
